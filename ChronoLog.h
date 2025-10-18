8954c070b7ccba245f32870eea3aa789fcbf7909
Hamas Saeed <hamasaeed888@gmail.com>
2025-10-18 16:29:16 +0500

    arduino lib registery fix

---
+++ ChronoLog.h
@@ -0,0 +1,16 @@
+/*
+ ============================================================================================================================================
+ * ChronoLog.h - Root-level wrapper for Arduino library compliance
+ * Author:      Hamas Saeed
+ * Version:     Rev_1.1.0
+ * Brief:       This wrapper header includes the actual ChronoLog implementation from include/ChronoLog.h
+ *              It exists at the root level to satisfy Arduino library specification requirements.
+ ============================================================================================================================================
+ */
+
+#ifndef CHRONOLOG_H
+#define CHRONOLOG_H
+
+#include "include/ChronoLog.h"
+
+#endif // CHRONOLOG_H
